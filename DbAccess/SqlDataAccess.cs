1be8925abb626d8a17cebe51ef47a0b55a1e13b3
vathai <campfiraga@gmail.com>
2023-09-19 22:00:08 -0700

    Added LoadData and SaveData DbAccess methods with interface using Dapper

--- DbAccess/SqlDataAccess.cs
+++ DbAccess/SqlDataAccess.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace DamageCalcLibrary.DbAccess
 {
-    public class SqlDataAccess
+    public class SqlDataAccess : ISqlDataAccess
     {
+        private readonly IConfiguration _configuration;
+
+        public SqlDataAccess(IConfiguration _config)
+        {
+            _configuration = _config;
+        }
+        public async Task<IEnumerable<T>> LoadData<T, U>(
+            string storedProcedure,
+            U parameters,
+            string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+        public async Task SaveData<T>(
+            string storedProcedure,
+            T parameters,
+            string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+
+            await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
     }
 }
