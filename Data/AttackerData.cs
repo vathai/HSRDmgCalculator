48e141345a9fb1ee8c90673c6310f02e66bd215a
vathai <campfiraga@gmail.com>
2023-09-20 22:06:56 -0700

    added AttackerData class(middle man for talking to sql, not accessing sql directly)

---
+++ Data/AttackerData.cs
@@ -0,0 +1,21 @@
+﻿using DamageCalcLibrary.DbAccess;
+using DamageCalcLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageCalcLibrary.Data;
+public class AttackerData
+{
+    private readonly ISqlDataAccess _db;
+
+    public AttackerData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<Attacker>> GetAttackers() =>
+        _db.LoadData<Attacker, dynamic>("dbo.spAttacker_GetAll", new { });
+}
